5f0ae38cda51fd6fb046cef6ffabca101c8e4b77
chrisp <krzysztof.pysz99@gmail.com>
2020-04-17 21:44:56 +0200

    init

---
+++ app/src/main/java/com/example/fae/ExampleItem.kt
@@ -0,0 +1,4 @@
+package com.example.fae
+
+data class ExampleItem(val imageResource: Int, val text1: String, val text2: String) {
+}
--- app/src/main/java/com/example/fae/HomeFragment.kt
+++ app/src/main/java/com/example/fae/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -15,8 +17,28 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val exampleList=generateDummyList(500)
+        recycler_view.adapter=ExampleAdapter(exampleList)
+        recycler_view.layoutManager=LinearLayoutManager(activity)
+    }
+    private fun generateDummyList(size: Int): List<ExampleItem>{
+        val list=ArrayList<ExampleItem>()
+        for (i in 0 until size){
+            val drawable = when (i%3){
+                0 -> R.drawable.ic_settings_input_antenna_black_24dp
+                1 -> R.drawable.ic_subway_black_24dp
+                else -> R.drawable.ic_thumb_up_black_24dp
+            }
+            val item=ExampleItem(drawable, "Item $i","Line 2")
+            list+=item
+        }
+        return list
+    }
+
 
 }
