7e010aaa0cf04ed74882545254c44376ea3ccbb5
chrisp <krzysztof.pysz99@gmail.com>
2020-04-22 20:58:48 +0200

    timer implemented

--- app/src/main/java/com/example/fae/HomeFragment.kt
+++ app/src/main/java/com/example/fae/HomeFragment.kt
@@ -2,11 +2,14 @@ package com.example.fae
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,14 +18,17 @@ import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.example_item.*
 import kotlinx.android.synthetic.main.example_item.view.*
+import kotlinx.android.synthetic.main.example_item.view.card
 import kotlinx.android.synthetic.main.fragment_home.*
+import net.theluckycoder.expandablecardview.ExpandableCardView
+import java.util.Locale.filter
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
-
     private var adapter: ItemFirestoreRecyclerAdapter? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,33 +39,41 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        recycler_view.layoutManager=LinearLayoutManager(activity)
+        recycler_view.layoutManager = LinearLayoutManager(activity)
         val rootRef = FirebaseFirestore.getInstance()
         val query = rootRef.collection("zdarzenia").orderBy("text1", Query.Direction.ASCENDING)
-        val options = FirestoreRecyclerOptions.Builder<ExampleItem>().setQuery(query, ExampleItem::class.java).build()
-        adapter=ItemFirestoreRecyclerAdapter(options)
-        recycler_view.adapter=adapter
+        val options =
+            FirestoreRecyclerOptions.Builder<ExampleItem>().setQuery(query, ExampleItem::class.java)
+                .build()
+        adapter = ItemFirestoreRecyclerAdapter(options)
+        recycler_view.adapter = adapter
     }
 
-    private inner class ExampleViewHolder internal constructor(private val view: View) : RecyclerView.ViewHolder(view) {
-        internal fun setText(text1: String,text2: String) {
-            val textView1 = view.findViewById<TextView>(R.id.text_view1)
-            val textView2 = view.findViewById<TextView>(R.id.text_view2)
-            textView1.text = text1
-            textView2.text = text2
 
+    private inner class ExampleViewHolder internal constructor(private val view: View) :
+        RecyclerView.ViewHolder(view) {
+        internal fun setText(text1: String, text2: String) {
+            val card = view.findViewById<ExpandableCardView>(R.id.card)
+            card!!.cardTitle = text1
+            card.cardDescription = text2
         }
     }
 
-    private inner class ItemFirestoreRecyclerAdapter internal constructor(options: FirestoreRecyclerOptions<ExampleItem>) : FirestoreRecyclerAdapter<ExampleItem, ExampleViewHolder>(options) {
-        override fun onBindViewHolder(ExampleViewHolder: ExampleViewHolder, position: Int, ExampleItem: ExampleItem) {
-            ExampleViewHolder.setText(ExampleItem.text1,ExampleItem.text2)
+    private inner class ItemFirestoreRecyclerAdapter internal constructor(options: FirestoreRecyclerOptions<ExampleItem>) :
+        FirestoreRecyclerAdapter<ExampleItem, ExampleViewHolder>(options) {
+        override fun onBindViewHolder(
+            ExampleViewHolder: ExampleViewHolder,
+            position: Int,
+            ExampleItem: ExampleItem
+        ) {
+            ExampleViewHolder.setText(ExampleItem.text1, ExampleItem.text2)
 
 
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.example_item, parent, false)
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.example_item, parent, false)
             return ExampleViewHolder(view)
         }
 
